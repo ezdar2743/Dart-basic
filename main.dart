f8a596d91cb5f2fab67dbee1350f28a314a24982
ezdar <ezdar2743@gmail.com>
2023-03-12 16:27:49 +0900

    dynamic type

--- main.dart
+++ main.dart
@@ -3,4 +3,16 @@ void main() {
   var name = 'Soku';
   name = 'Ezdar';
   String s_name = 'Soku';
+
+  // Dynamic Type
+  var age;
+  dynamic gender;
+  gender = true;
+  gender = 'woman';
+  age = 12;
+  age = 'twelve';
+  age = true;
+  if (age is String) {
+    name.length; // Dart knows name is String type.
+  }
 }
